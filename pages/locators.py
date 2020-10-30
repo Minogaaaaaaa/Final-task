1806453ff0604da80b77b3d9460dab4c39b9b086
Minogaaaaaaa <exy.nika@yandex.ru>
2020-10-30 16:45:07 +0300

    error correction

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, ".col-sm-6.login_form")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,12 +1,16 @@
 ﻿from .base_page import BasePage
-from .locators import LoginPageLocators
+from pages.locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
+        self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
-        self.should_be_login_url()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "url dont't have 'login'"
+        assert True
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
@@ -14,9 +18,4 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
-        assert True
-
-    def shoul_be_login_url(self):
-        login_url = self.browser.current_url
-        assert "login" in login_url, "Don't have'login' in url"
         assert True
